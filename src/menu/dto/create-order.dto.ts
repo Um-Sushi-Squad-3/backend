a90833250ab208aa6e7ceb160c3bb51e4b9bfcaa
Ramos <mios@gft.com>
2025-07-21 10:50:42 -0300

    Revert "Merge pull request #2 from Um-Sushi-Squad-3/DTOValidator"
    This reverts commit 7889249a60f19f15670cd770a4a12c07ec3d1519, reversing
    changes made to 68a5ea13a9b3f41a5a21f5a2fccd82170e4af77d.

--- src/menu/dto/create-order.dto.ts
+++ src/menu/dto/create-order.dto.ts
@@ -2,42 +2,25 @@
 // Eles são usados na rota POST /menu/order
 
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsArray, ValidateNested, IsNumber, Min } from 'class-validator';
-import { Type } from 'class-transformer';
 
 export class OrderItem {
   @ApiProperty({ example: 1, description: 'ID do produto' })
-  @IsNotEmpty()
-  @IsNumber()
   productId!: number;
 
   @ApiProperty({ example: 2, description: 'Quantidade do produto' })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
   quantity!: number;
 }
 
 export class CreateOrderDto {
   @ApiProperty({ example: 'Gil do Vigor', description: 'Nome do cliente' })
-  @IsNotEmpty()
-  @IsString()
   customerName!: string;
 
   @ApiProperty({ example: '999999999', description: 'Telefone do cliente' })
-  @IsNotEmpty()
-  @IsString()
   customerPhone!: string;
 
   @ApiProperty({ example: 'Rua das Flores, 123', description: 'Endereço para entrega' })
-  @IsNotEmpty()
-  @IsString()
   address!: string;
 
   @ApiProperty({ type: [OrderItem], description: 'Lista de itens do pedido' })
-  @IsNotEmpty()
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => OrderItem)
   items!: OrderItem[];
 }
