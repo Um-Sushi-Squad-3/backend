0dd719188dfad1a9a85e25ef62ddd5d4d1a8cebc
Mércia Ramos <ramosmercia2002@gmail.com>
2025-07-21 11:54:10 -0300

    fix: mensagem amigavel para usuário na aplicação inicial render 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-// Serve como o ponto de entrada da aplicação e organiza os módulos internos
-
 import { Module } from '@nestjs/common';
 import { MenuModule } from './menu/menu.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [MenuModule],
+  controllers: [AppController],     
+  providers: [AppService],          
 })
 export class AppModule {}
