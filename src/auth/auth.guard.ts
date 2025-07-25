f2e7d0cb24ffff3a5ea4c03fd39080e579407e41
Ramos <mios@gft.com>
2025-07-25 17:21:05 -0300

    Revert "feat: add basic logic for authentication in order endpoint"
    This reverts commit bebb499573baae90055d46c321de36f65f73ea16.

--- src/auth/auth.guard.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
-import { AuthGuard as PassportAuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class AuthGuard extends PassportAuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    return super.canActivate(context);
-  }
-}
