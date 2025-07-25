aacb3b7639d6f4467a73af407f5b606c2d9677fe
Ramos <mios@gft.com>
2025-07-25 17:28:26 -0300

    Revert "Merge pull request #6 from Um-Sushi-Squad-3/adrielle/add_jwt"
    This reverts commit 1649ce1f5d29eb7d0ec09e2167bfa298aba9e00f, reversing
    changes made to 2124822a730394e3ed492bacc8fa597d592bd29f.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MenuModule } from './menu/menu.module';
-import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MenuModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MenuModule],
+  controllers: [AppController],     
+  providers: [AppService],          
 })
 export class AppModule {}
